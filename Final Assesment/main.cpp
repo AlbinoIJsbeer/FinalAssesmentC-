8d983e74a6096b6fbeee877a941cd873efe4d433
Tom <tomdekinkelder@live.nl>
2019-10-08 08:42:49 +0100

    Added basic input/output

--- Final Assesment/main.cpp
+++ Final Assesment/main.cpp
@@ -1,7 +1,10 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() 
 {	
+	char name[15];
+	std::cout << "Type your name please: \n";
+	std::cin >> name;
+	std::cout << "Hello " << name << "\n";
 	return 0;
 }

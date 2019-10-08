430e24fda2836bf35d12eed4291a53fb140ba78d
Tom <tomdekinkelder@live.nl>
2019-10-08 09:05:24 +0100

    Added more input/output

--- Final Assesment/main.cpp
+++ Final Assesment/main.cpp
@@ -3,8 +3,11 @@
 int main() 
 {	
 	char name[15];
+	char username[30];
 	std::cout << "Type your name please: \n";
 	std::cin >> name;
-	std::cout << "Hello " << name << "\n";
+	std::cout << "Type your username please: \n";
+	std::cin >> username;
+	std::cout << "Hello " << name << ", AKA: " << username << "\n";
 	return 0;
 }

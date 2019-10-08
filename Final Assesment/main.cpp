106a26e87b2c4a28256ea27f396e1d512b2969e2
Tom <tomdekinkelder@live.nl>
2019-10-08 09:57:53 +0100

    Added Attack and Heal buttons

--- Final Assesment/main.cpp
+++ Final Assesment/main.cpp
@@ -3,9 +3,18 @@
 
 int main() 
 {	
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1024, 512), "Ponkerman!");
+
+	sf::RectangleShape attack(sf::Vector2f(128.0f, 64.0f));
+	attack.setFillColor(sf::Color::Red);
+	attack.setPosition(0.0f, 448.0f);
+
+	sf::RectangleShape heal(sf::Vector2f(128.0f, 64.0f));
+	heal.setFillColor(sf::Color::Blue);
+	heal.setPosition(attack.getPosition().x + attack.getSize().x, attack.getPosition().y);
+
+	sf::Vector2i mousePos;
+	bool Mousepressed = false;
 
 	while(window.isOpen())
 	{
@@ -16,26 +25,37 @@ int main()
 				window.close();
 		}
 
+		if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+		{
+			
+			mousePos = sf::Mouse::getPosition(window);
+			if(Mousepressed)
+			{
+				continue;
+			}
+			else if(!Mousepressed)
+			{ 
+				if(mousePos.x > attack.getPosition().x && mousePos.x < attack.getPosition().x + attack.getSize().x
+				   && mousePos.y > attack.getPosition().y && mousePos.y < attack.getPosition().y + attack.getSize().y)
+				{
+					std::cout << "You attacked!\n";
+				} else if(mousePos.x > heal.getPosition().x && mousePos.x < heal.getPosition().x + heal.getSize().x
+						  && mousePos.y > heal.getPosition().y && mousePos.y < heal.getPosition().y + heal.getSize().y)
+				{
+					std::cout << "You healed!\n";
+				}
+				Mousepressed = true;
+			}
+			continue;
+		}
+
+		Mousepressed = false;
 		window.clear();
-		window.draw(shape);
+		window.draw(attack);
+		window.draw(heal);
 		window.display();
 	}
 
 	return 0;
 	
-	
-	
-	
-	
-	
-	
-	
-	/*char name[15];
-	char username[30];
-	std::cout << "Type your name please: \n";
-	std::cin >> name;
-	std::cout << "Type your username please: \n";
-	std::cin >> username;
-	std::cout << "Hello " << name << ", AKA: " << username << "\n";
-	return 0;*/
 }
